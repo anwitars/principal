df8ea67743d98e46b190b740bf4e79e18e8f9f5b
Gábor Maksa <maksa22242@gmail.com>
2024-01-22 16:39:55 +0100

    env: using mocha for testing

--- tests/time.test.ts
+++ tests/time.test.ts
@@ -1,31 +1,34 @@
+import assert from "assert";
 import { PrincipalTime } from "../src/time";
 
-test("test toString method on PrincipalTime", () => {
-  const date = new Date("2021-01-01 00:00:00");
-  const principalTime = new PrincipalTime(date);
+describe("test PrincipalTime", () => {
+  it("test toString method on PrincipalTime", () => {
+    const date = new Date("2021-01-01 00:00:00");
+    const principalTime = new PrincipalTime(date);
 
-  const expected = "2021. 01. 01. 0:00:00";
-  const actual = String(principalTime);
+    const expected = "2021. 01. 01. 0:00:00";
+    const actual = String(principalTime);
 
-  expect(actual).toBe(expected);
-});
+    assert.equal(actual, expected);
+  });
 
-test("test offset method forwards on PrincipalTime", () => {
-  const date = new Date("2020-12-31 23:59:00");
-  const principalTime = new PrincipalTime(date);
+  it("test offset method forwards on PrincipalTime", () => {
+    const date = new Date("2020-12-31 23:59:00");
+    const principalTime = new PrincipalTime(date);
 
-  const expected = "2021. 01. 01. 0:00:00";
-  const actual = String(principalTime.offset({ minutes: 1 }));
+    const expected = "2021. 01. 01. 0:00:00";
+    const actual = String(principalTime.offset({ minutes: 1 }));
 
-  expect(actual).toBe(expected);
-});
+    assert.equal(actual, expected);
+  });
 
-test("test offset method backwards on PrincipalTime", () => {
-  const date = new Date("2021-01-01 00:00:00");
-  const principalTime = new PrincipalTime(date);
+  it("test offset method backwards on PrincipalTime", () => {
+    const date = new Date("2021-01-01 00:00:00");
+    const principalTime = new PrincipalTime(date);
 
-  const expected = "2020. 12. 31. 23:59:00";
-  const actual = String(principalTime.offset({ minutes: -1 }));
+    const expected = "2020. 12. 31. 23:59:00";
+    const actual = String(principalTime.offset({ minutes: -1 }));
 
-  expect(actual).toBe(expected);
+    assert.equal(actual, expected);
+  });
 });
